c5357544690a2c7ad47fbbd48e7b6756119c6b66
Franco Bonilla <bonilla.franco484@gmail.com>
2025-09-25 02:08:21 -0600

    Progress on personalization

--- assets/js/script.js
+++ assets/js/script.js
@@ -143,17 +143,21 @@ const pages = document.querySelectorAll("[data-page]");
 // add event to all nav link
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
+    // remove 'active' from all links
+    navigationLinks.forEach(link => link.classList.remove("active"));
+    pages.forEach(page => page.classList.remove("active"));    
 
-    for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
-        pages[i].classList.add("active");
-        navigationLinks[i].classList.add("active");
+    // add 'active' to clicked link
+    this.classList.add("active")
+
+    // activate matching page
+    const pgName = this.innerText.toLowerCase();
+    pages.forEach(pg => {
+      if (pg.dataset.page === pgName) {
+        pg.classList.add("active");
         window.scrollTo(0, 0);
-      } else {
-        pages[i].classList.remove("active");
-        navigationLinks[i].classList.remove("active");
       }
-    }
+    });
 
   });
 }
